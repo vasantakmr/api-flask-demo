39e78b7d044bd58efc6ba51e6a451d4d57b90a83
vasantakmr <vasantakmr6@gmail.com>
2021-05-28 19:03:26 +0530

    dummy commit

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ r"/*":{
 @app.route('/data', methods=['GET'])
 def getData():
     try:
-        return jsonify([{"html_url":"http://google.com","name":"ASPlus-Golf","description":"dummy","owner":{"login":"mark","repos_url":"https://api.github.com/users/mark/repos","site_admin":"false"}}, {"name":"ASPlus-Golf","description":"dummy","owner":{"login":"mark","repos_url":"https://api.github.com/users/mark/repos","site_admin":"false"}}, {"name":"ASPlus-Golf","description":"dummy","owner":{"login":"mark","repos_url":"https://api.github.com/users/mark/repos","site_admin":"false"}}, {"name":"ASPlus-Golf","description":"dummy","owner":{"login":"mark","repos_url":"https://api.github.com/users/mark/repos","site_admin":"false"}},{"name":"ASPlus-Golf","description":"dummy","owner":{"login":"mark","repos_url":"https://api.github.com/users/mark/repos","site_admin":"false"}}])
+        return jsonify([{"html_url":"http://google.com","name":"fdfdfdfd-Golf","description":"dsdfdfdfd","owner":{"login":"mark","repos_url":"https://api.github.com/users/mark/repos","site_admin":"false"}}, {"name":"Golf","description":"dummy","owner":{"login":"mark","repos_url":"https://api.github.com/users/mark/repos","site_admin":"false"}}, {"name":"ASPlus-Golf","description":"dummy","owner":{"login":"mark","repos_url":"https://api.github.com/users/mark/repos","site_admin":"false"}}, {"name":"ASPlus-Golf","description":"dummy","owner":{"login":"mark","repos_url":"https://api.github.com/users/mark/repos","site_admin":"false"}},{"name":"ASPlus-Golf","description":"dummy","owner":{"login":"mark","repos_url":"https://api.github.com/users/mark/repos","site_admin":"false"}}])
     except Exception as e:
         return f"error occurred"
 
